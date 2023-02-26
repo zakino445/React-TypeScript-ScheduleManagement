dfbf848cd2c604904963e081f2042ab70822ddec
zaki <zaki20020302@gmail.com>
2023-02-26 18:23:39 +0900

    chore: title属性内のコメント修正

--- src/components/todo/features/input/Input.tsx
+++ src/components/todo/features/input/Input.tsx
@@ -4,7 +4,7 @@ import "./input.css";
 export const Input: FC = () => {
   return (
     <div className="input-box">
-      <input type="text" placeholder="入力" title="挑戦することを入力" className="input" />
+      <input type="text" placeholder="入力" title="挑戦すること" className="input" />
     </div>
   );
 };

10d53c3e538d0944b12dd44432e44bfbb32fa049
zaki <zaki20020302@gmail.com>
2023-02-28 15:37:35 +0900

    chore: 達成エリアUI作成のクラス名修正

--- src/components/todo/features/ChallengeArea/ChallengeArea.tsx
+++ src/components/todo/features/ChallengeArea/ChallengeArea.tsx
@@ -7,8 +7,8 @@ export const ChallengeArea: FC = () => {
     <div className="challengeArea">
       <p className="challengeArea-title">挑戦中</p>
       <ul className="challengeArea-list">
-        <li className="challengeArea-list-text">
-          <span className="challengeArea-list-text-size">仮置き</span>
+        <li className="challengeArea-list-item">
+          <span className="challengeArea-list-text">仮置き</span>
           <Button text={"達成"} />
           <Button text={"失敗"} />
         </li>

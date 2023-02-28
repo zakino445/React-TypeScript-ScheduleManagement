fd0d6238bc1899f137ddc1286db3ceca4d525643
zaki <zaki20020302@gmail.com>
2023-02-28 16:29:15 +0900

    feat: #19 入力機能追加

--- src/components/todo/features/AddField/AddField.tsx
+++ src/components/todo/features/AddField/AddField.tsx
@@ -1,13 +1,18 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Button } from "../Button/Button";
 import { Input } from "../Input/Input";
 import "./AddField.css";
 
 export const AddField: FC = () => {
+  const [addText, setAddText] = useState("");
+
+  const onChangeAddText = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setAddText(event.target.value);
+
   return (
     <div className="addField-wrapper">
       <div className="addField-input-wrapper">
-        <Input />
+        <Input onChangeAddText={onChangeAddText} addText={addText} />
       </div>
       <div className="addField-button-wrapper">
         <Button text={"追加"} />
--- src/components/todo/features/Input/Input.tsx
+++ src/components/todo/features/Input/Input.tsx
@@ -1,6 +1,20 @@
 import { FC } from "react";
 import "./Input.css";
 
-export const Input: FC = () => {
-  return <input type="text" placeholder="入力" title="挑戦すること" className="input" />;
+type Props = {
+  addText: string;
+  onChangeAddText: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const Input: FC<Props> = ({ addText, onChangeAddText }) => {
+  return (
+    <input
+      type="text"
+      placeholder="入力"
+      value={addText}
+      onChange={onChangeAddText}
+      title="挑戦すること"
+      className="input"
+    />
+  );
 };
